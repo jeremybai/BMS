412714ca629681a7f05f7db8f90d2e1a605ed742
jeremybai <jeremybaiit@gmail.com>
2013-12-07 14:00:06 +0800

    dummy

--- bookms/settings.py
+++ bookms/settings.py
@@ -4,6 +4,3 @@ TEMPLATE_DIRS += (os.path.join(  os.path.dirname(__file__), 'templates') ,)
 
 import os
 TEMPLATE_DIRS += (os.path.join(  os.path.dirname(__file__), 'templates') ,)
-
-
-#branch test1  
